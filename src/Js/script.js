312240148ca3ed77dbd1278e19c49940940257e0
rakibul44 <mdrakib76634@gmail.com>
2025-07-10 01:28:26 +0600

    pages and footer added

--- src/Js/script.js
+++ src/Js/script.js
@@ -5,3 +5,17 @@ function openContact() {
 function viewServices() {
   alert("Services Coming Soon!");
 }
+
+// live location
+ function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        alert("Geolocation is not supported by this browser.");
+      }
+    }
+
+    function showPosition(position) {
+      alert("Your current location is:\nLatitude: " + position.coords.latitude + "\nLongitude: " + position.coords.longitude);
+      // Optional: Use reverse geocoding API to set actual city in the location dropdown
+    }
